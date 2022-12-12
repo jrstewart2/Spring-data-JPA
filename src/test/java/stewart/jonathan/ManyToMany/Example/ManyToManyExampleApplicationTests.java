d697749a8400d860e9e4077d3dfc7c5b6e7975af
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-12-12 16:30:59 +0000

    template committed

---
+++ src/test/java/stewart/jonathan/ManyToMany/Example/ManyToManyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package stewart.jonathan.ManyToMany.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
